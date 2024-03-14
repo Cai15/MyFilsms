caa949c88154ce1c9f967285e7f9926259342611
Cai15 <andryxa-96@mail.ru>
2024-03-14 20:27:18 +0300

    Переведите всю асинхронную работу на Корутины (запросы в БД, получение данных для UI и т.д.)

--- app/src/main/java/com/example/myfilsms/data/MainRepository.kt
+++ app/src/main/java/com/example/myfilsms/data/MainRepository.kt
@@ -1,6 +1,5 @@
 package com.example.myfilsms.data
 
-import androidx.lifecycle.LiveData
 import com.example.myfilsms.data.dao.FilmDao
 import com.example.myfilsms.data.entity.Film
 import kotlinx.coroutines.flow.Flow
@@ -9,12 +8,9 @@ import java.util.concurrent.Executors
 class MainRepository(private val filmDao: FilmDao) {
 
     fun putToDb(films: List<Film>) {
-        //Запросы в бд должны быть в отдельном потоке
-        fun putToDb(films: List<Film>) {
-            filmDao.insertAll(films)
-        }
-
-        fun getAllFromDB(): Flow<List<Film>> = filmDao.getCachedFilms()
-
+        filmDao.insertAll(films)
     }
+
+    fun getAllFromDB(): Flow<List<Film>> = filmDao.getCachedFilms()
+
 }
