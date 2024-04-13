639f8ce898894847bd72526188c2887125308b6b
Cai15 <andryxa-96@mail.ru>
2024-04-13 22:48:25 +0300

    Создайте BroadcastReceiver, который будет слушать низкий уровень заряда батареи и подключение кабеля зарядки. По этим событиям он должен выводить соответствующие Тосты или ещё какие-то предупреждения. + включение тёмной темы

--- app/src/main/java/com/example/myfilsms/receivers/ConnectionChecker.kt
+++ app/src/main/java/com/example/myfilsms/receivers/ConnectionChecker.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 
 class ConnectionChecker : BroadcastReceiver() {
 
@@ -14,6 +15,8 @@ class ConnectionChecker : BroadcastReceiver() {
         when (intent.action) {
             //Если пришел низкий заряд батарее
             Intent.ACTION_BATTERY_LOW -> Toast.makeText(context, "Батарея разряжена", Toast.LENGTH_SHORT).show()
+            //Веключенние темнйо темы при разряже батареи
+            Intent.ACTION_BATTERY_LOW -> MODE_NIGHT_FOLLOW_SYSTEM
             //Если пришло подключение к зараядке
             Intent.ACTION_POWER_CONNECTED -> Toast.makeText(context, "Зарядка подключена", Toast.LENGTH_SHORT).show()
         }
